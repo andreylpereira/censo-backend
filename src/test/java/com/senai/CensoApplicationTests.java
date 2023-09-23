db9c84fb4143d1fc3a6160e26f95e176a13a82b1
Andrey Luiz Pereira <andreylpereira@gmail.com>
2023-09-23 20:05:15 -0300

    first commit

---
+++ src/test/java/com/senai/CensoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CensoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
